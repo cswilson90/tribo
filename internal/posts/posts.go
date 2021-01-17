4c77079c762a829baffdb6bf7d1ed76aa89a161d
Chris Wilson <cswilson90@gmail.com>
2021-01-17 18:03:13 +0000

    Split post detection into different function so it can be expanded

--- internal/posts/posts.go
+++ internal/posts/posts.go
@@ -2,6 +2,7 @@ package posts
 
 import (
 	"container/list"
+	"fmt"
 	"io/ioutil"
 	"path/filepath"
 	"regexp"
@@ -19,6 +20,7 @@ type Post struct {
 	dir string
 }
 
+// FindPosts recursively searches a directory for posts.
 func FindPosts(baseDir string) Posts {
 	log.Infof("Looking for posts recursivly in '%v'", baseDir)
 
@@ -34,22 +36,20 @@ func FindPosts(baseDir string) Posts {
 
 		log.Debugf("Looking for posts in '%v'", nextDir)
 
+		// If directory contains a post add to the list and stop exploring it
+		post, err := newPost(nextDir)
+		if err == nil {
+			posts = append(posts, post)
+			log.Debugf("Found post in '%v'", nextDir)
+			continue
+		}
+
+		// Recursively check sub-direcotories for posts
 		fileList, err := ioutil.ReadDir(nextDir)
 		if err != nil {
 			log.Warnf("Could not list files in directory '%v':"+err.Error(), nextDir)
 			continue
 		}
-
-		// If the directory contains a metadata file it's a post directory
-		for _, file := range fileList {
-			if metaDataMatch.MatchString(file.Name()) {
-				posts = append(posts, &Post{nextDir})
-				log.Debugf("Found post in '%v'", nextDir)
-				continue
-			}
-		}
-
-		// Recursively check sub-direcotories for posts
 		for _, file := range fileList {
 			if file.IsDir() {
 				newDir := filepath.Join(nextDir, file.Name())
@@ -60,4 +60,25 @@ func FindPosts(baseDir string) Posts {
 
 	log.Infof("Found %v posts in '%v'", len(posts), baseDir)
 	return posts
+}
+
+func newPost(dir string) (*Post, error) {
+	fileList, err := ioutil.ReadDir(dir)
+	if err != nil {
+		return nil, err
+	}
+
+	// If the directory contains a metadata file it's a post directory
+	var metadataFile string
+	for _, file := range fileList {
+		if metaDataMatch.MatchString(file.Name()) {
+			metadataFile = filepath.Join(dir, file.Name())
+		}
+	}
+
+	if metadataFile == "" {
+		return nil, fmt.Errorf("Dir '%v' is not a post directory", dir)
+	}
+
+	return &Post{dir}, nil
 }
