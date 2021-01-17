c4433012bd79308fabac57642a6706a0087b7529
Chris Wilson <cswilson90@gmail.com>
2021-01-17 17:32:07 +0000

    Convert post finding code to not use recursion

--- internal/posts/posts.go
+++ internal/posts/posts.go
@@ -1,6 +1,7 @@
 package posts
 
 import (
+	"container/list"
 	"io/ioutil"
 	"path/filepath"
 	"regexp"
@@ -21,42 +22,51 @@ type Post struct {
 func FindPosts(baseDir string) Posts {
 	log.Infof("Looking for posts recursivly in '%v'", baseDir)
 
-	posts := findPosts(baseDir, make(DirSet))
+	// Save which directories we've seen before to avoid circular references
+	// causing an infinite loop
+	seenLists := make(DirSet)
+
+	toProcess := list.New()
+	toProcess.PushBack(baseDir)
+	seenLists[baseDir] = true
+
+	posts := make(Posts, 0)
+
+	for toProcess.Len() > 0 {
+		nextElement := toProcess.Front()
+		nextDir := nextElement.Value.(string)
+		toProcess.Remove(nextElement)
+
+		log.Debugf("Looking for posts in '%v'", nextDir)
+
+		fileList, err := ioutil.ReadDir(nextDir)
+		if err != nil {
+			log.Warnf("Could not list files in directory '%v':"+err.Error(), nextDir)
+			continue
+		}
+
+		// If the directory contains a metadata file it's a post directory
+		for _, file := range fileList {
+			if metaDataMatch.MatchString(file.Name()) {
+				posts = append(posts, &Post{nextDir})
+				continue
+			}
+		}
+
+		// Recursively check sub-direcotories for posts
+		for _, file := range fileList {
+			if file.IsDir() {
+				newDir := filepath.Join(nextDir, file.Name())
+				if !seenLists[newDir] {
+					toProcess.PushBack(newDir)
+					seenLists[newDir] = true
+				}
+			}
+		}
+
+		log.Debugf("Found %v posts in '%v'", len(posts), nextDir)
+	}
 
 	log.Infof("Found %v posts in '%v'", len(posts), baseDir)
-	return posts
-}
-
-func findPosts(baseDir string, seenDirs DirSet) Posts {
-	log.Debugf("Looking for posts in '%v'", baseDir)
-
-	// Stop if we've seen this directory before (prevents infinite loops due to circular references)
-	if seenDirs[baseDir] {
-		return Posts{}
-	}
-	seenDirs[baseDir] = true
-
-	fileList, err := ioutil.ReadDir(baseDir)
-	if err != nil {
-		log.Warnf("Could not list files in directory '%v':"+err.Error(), baseDir)
-		return Posts{}
-	}
-
-	// If the directory contains a metadata file it's a post directory
-	for _, file := range fileList {
-		if metaDataMatch.MatchString(file.Name()) {
-			return Posts{&Post{baseDir}}
-		}
-	}
-
-	// Recursively check sub-direcotories for posts
-	posts := make(Posts, 0)
-	for _, file := range fileList {
-		if file.IsDir() {
-			posts = append(posts, findPosts(filepath.Join(baseDir, file.Name()), seenDirs)...)
-		}
-	}
-
-	log.Debugf("Found %v posts in '%v'", len(posts), baseDir)
 	return posts
 }
